eb682af70535eda8b78eef5c8ea0e766fb5c7130
toqitahmid <rubaiathali@gmail.com>
2026-09-16 23:06:20 +0600

    setup cloudinary and multer

---
+++ src/models/video.model.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true
+        },
+        thumbnail: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: ture,
+        },
+        duration: {
+            type: Number,
+            required: ture
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+    },
+    {
+        timeseries: true
+    }
+)
+
+Video.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model('Video', videoSchema);
