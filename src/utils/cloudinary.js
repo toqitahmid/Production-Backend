8ff9ef6ba6179f863da8f4059662152605a9a50f
toqitahmid <rubaiathali@gmail.com>
2026-09-16 22:57:16 +0600

    install cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,41 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({ 
+  cloud_name: CLOUDINARY_CLOUD_NAME, 
+  api_key: CLOUDINARY_API_KEY, 
+  api_secret: CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file in cloudinary
+        const res = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file has been uploaded successfull
+        console.log("file is uploaded on cloudinary", res.url)
+        fs.unlinkSync(localFilePath)
+        return response;
+    }
+    catch (err) {
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation got faild
+        return null;
+    }
+}
+
+export{uploadOnCloudinary}
+
+
+
+
+
+// cloudinary.v2.uploader
+// .upload("dog.mp4", {
+//   resource_type: "video", 
+//   public_id: "my_dog",
+//   overwrite: true, 
+//   notification_url: "https://mysite.example.com/notify_endpoint"})
+// .then(result=>console.log(result));
