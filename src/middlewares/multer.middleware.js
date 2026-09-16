dd0a83ff56352a4391025c8f23598da1ad200ddd
toqitahmid <rubaiathali@gmail.com>
2026-09-16 23:13:16 +0600

    feat: add Cloudinary upload utility and Multer file upload middleware

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -4,10 +4,6 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "./public/temp")
     },
-<<<<<<< HEAD
-    
-=======
->>>>>>> 8ff9ef6ba6179f863da8f4059662152605a9a50f
     filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
